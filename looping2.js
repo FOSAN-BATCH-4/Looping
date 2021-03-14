214bbecf911eb3269591ad970a5a61ac8cf4b4d9
Qlats890 <joshepreaper@gmail.com>
2021-03-14 23:14:53 +0700

    Looping Assignment

---
+++ looping2.js
@@ -0,0 +1,20 @@
+/**
+ * Membuat perulangan menggunakan for
+ * jalan maju mundur (2,4,6,6,4,2)
+ * total kebawah ada 20
+ * GO !
+ */
+for(let looping1 = 0;looping1<=20;looping1++){
+    if(looping1!=0){
+        console.log(looping1+' I love coding');
+    }else{
+        console.log('LOOPING PERTAMA');
+    }
+}
+for(let looping2 = 21;looping2>=1;looping2--){
+    if(looping2!=21){
+        console.log(looping2+' I love coding');
+    }else{
+        console.log('LOOPING KEDUA');
+    }
+}
