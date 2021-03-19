3a2a0de3d77e510844709b3d7d54561013aafe64
byu224 <nadyaawibowo@gmail.com>
2021-03-19 11:37:01 +0700

    Nadya

---
+++ loop3.js
@@ -0,0 +1,9 @@
+console.log('LOOPING PERTAMA');
+for(let i=1;i<21;i++){
+    console.log(i+' - I love coding')
+}
+
+console.log('LOOPING KEDUA');
+for(let j=20;j>=1;j--){
+    console.log(j+' - I will become fullstack developer')
+}
